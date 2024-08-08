41704de4481a43ee30c2cd0dffee5f321d501d7c
Michael <marchbold@users.noreply.github.com>
2024-08-08 17:21:31 +1000

    feat(assets): update ios project to support latest xcode app icon sizes

--- src/generate-assets-zip.sh
+++ src/generate-assets-zip.sh
@@ -12,12 +12,12 @@ echo ${OUTPUTNAME}
 
 mkdir "${PACKAGEDIR}" 2>/dev/null
 
-rm -Rf "${SCRIPTDIR}/ios/build"
-xcodebuild -quiet -project "${SCRIPTDIR}/ios/AIRAssets.xcodeproj/" -scheme AIRAssets build
+rm -Rf "${SCRIPTDIR}/ios/Build"
+xcodebuild -quiet -project "${SCRIPTDIR}/ios/AIRAssets.xcodeproj/" -alltargets build
 
 find "${SCRIPTDIR}" -name .DS_Store -delete
 cp -Rf "${SCRIPTDIR}/ios/AIRAssets/Assets.xcassets" "${PACKAGEDIR}/." 
-cp -Rf "${SCRIPTDIR}/ios/Build/Products/Release-iphoneos/AIRAssets.app/Base.lproj/LaunchScreen.storyboardc" "${PACKAGEDIR}/."
+cp -Rf "${SCRIPTDIR}/ios/Build/Release-iphoneos/AIRAssets.app/Base.lproj/LaunchScreen.storyboardc" "${PACKAGEDIR}/."
 
 cd "${PACKAGEDIR}"
 rm -Rf "${WORKINGDIR}/${OUTPUTNAME}"
